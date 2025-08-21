e4c1274dc686e02a6fd80d2435f521be61063018
Sofiane Khenchelaoui <sofianekhenchelaoui1@gmail.com>
2025-08-22 00:45:09 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+  base: '/wissnails/', // 👈 VERY IMPORTANT: matches your repo name
+})
