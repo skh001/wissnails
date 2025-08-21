ad4bcfdd56a16c8bd6703295c949c6c0f6400b10
Sofiane Khenchelaoui <sofianekhenchelaoui1@gmail.com>
2025-08-22 01:21:37 +0200

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/wissnails', // 👈 VERY IMPORTANT: matches your repo name
+  base: '/wissnails/', // 👈 VERY IMPORTANT: matches your repo name
 })
